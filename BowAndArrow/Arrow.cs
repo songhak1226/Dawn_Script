cd2afc630ccdcb664ee7be71bdca7c06b8ab7ad3
songhak1226 <88295916+songhak1226@users.noreply.github.com>
2023-04-09 23:55:25 +0900

    Add files via upload

---
+++ BowAndArrow/Arrow.cs
@@ -0,0 +1,184 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Arrow : XRGrabInteractable
+{
+    TrailRenderer trailRenderer;
+    [SerializeField] ParticleSystem hitParticlePrefab;
+    [SerializeField] ParticleSystem hitParticlePrefab1;
+    AudioSource audioSource;
+    [Header("Settings")]
+    public float speed = 2000.0f;
+
+    [Header("Hit")]
+    public Transform tip = null;
+    public LayerMask layerMask = ~Physics.IgnoreRaycastLayer;
+
+    private new Collider collider = null;
+    private new Rigidbody rigidbody = null;
+
+    private Vector3 lastPosition = Vector3.zero;
+    private bool launched = false;
+
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+        collider = GetComponent<Collider>();
+        rigidbody = GetComponent<Rigidbody>();
+        trailRenderer = GetComponent<TrailRenderer>();
+        audioSource = GetComponent<AudioSource>();
+
+    }
+    private void traiilEnable()
+    {
+        trailRenderer.enabled = true;
+    }
+
+    protected override void OnSelectEntering(SelectEnterEventArgs args)
+    {
+        // Do this first, so we get the right physics values
+        if (args.interactor is XRDirectInteractor)
+            Clear();
+
+        // Make sure to do this
+        base.OnSelectEntering(args);
+    }
+
+    private void Clear()
+    {
+        SetLaunch(false);
+        TogglePhysics(true);
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        // Make sure to do this
+        base.OnSelectExited(args);
+
+        // If it's a notch, launch the arrow
+        if (args.interactor is Notch notch)
+            Launch(notch);
+    }
+
+    private void Launch(Notch notch)
+    {
+        // Double-check incase the bow is dropped with arrow socketed
+        if (notch.IsReady)
+        {
+            SetLaunch(true);
+            UpdateLastPosition();
+            ApplyForce(notch.PullMeasurer);
+            
+        }
+    }
+
+    private void SetLaunch(bool value)
+    {
+        collider.isTrigger = value;
+        launched = value;
+    }
+
+    private void UpdateLastPosition()
+    {
+        // Always use the tip's position
+        lastPosition = tip.position;
+    }
+
+    private void ApplyForce(PullMeasurer pullMeasurer)
+    {
+        // Apply force to the arrow
+        float power = pullMeasurer.PullAmount;
+        Vector3 force = transform.forward * (power * speed);
+        rigidbody.AddForce(force);
+    }
+
+    public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+    {
+        base.ProcessInteractable(updatePhase);
+
+        if (launched)
+        {
+            // Check for collision as often as possible
+            if (updatePhase == XRInteractionUpdateOrder.UpdatePhase.Dynamic)
+            {
+                if (CheckForCollision())
+                    launched = false;
+                    //audioSource.PlayOneShot(hitArrow);
+
+                UpdateLastPosition();
+            }
+
+            // Only set the direction with each physics update
+            if (updatePhase == XRInteractionUpdateOrder.UpdatePhase.Fixed)
+                SetDirection();
+        }
+    }
+
+    private void SetDirection()
+    {
+        // Look in the direction the arrow is moving
+        if (rigidbody.velocity.z > 0.5f)
+            transform.forward = rigidbody.velocity;
+    }
+
+    private bool CheckForCollision()
+    {
+        // Check if there was a hit
+        if (Physics.Linecast(lastPosition, tip.position, out RaycastHit hit, layerMask))
+        {
+            TogglePhysics(false);
+            ChildArrow(hit);
+            CheckForHittable(hit);
+        }
+
+        return hit.collider != null;
+    }
+
+    private void TogglePhysics(bool value)
+    {
+        // Disable physics for childing and grabbing
+        rigidbody.isKinematic = !value;
+        rigidbody.useGravity = value;
+    }
+
+    private void ChildArrow(RaycastHit hit)
+    {
+        // Child to hit object
+        Transform newParent = hit.collider.transform;
+        transform.SetParent(newParent);
+    }
+
+    private void CheckForHittable(RaycastHit hit)
+    {
+        // Check if the hit object has a component that uses the hittable interface
+        GameObject hitObject = hit.transform.gameObject;
+        IArrowHittable hittable = hitObject ? hitObject.GetComponent<IArrowHittable>() : null;
+        // If we find a valid component, call whatever functionality it has
+        if (hittable != null)
+        {
+            hittable.Hit(this);
+
+        }
+            
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "EnemyD")
+        {
+            other.SendMessage("OnHitArrow");   // 충돌 대상에 "OnHitBullet" 메시지
+            audioSource.Play();
+            Instantiate(hitParticlePrefab, transform.position, transform.rotation); // 총알 적중 지점에 연츨
+            Destroy(gameObject);    // 자신의 게임 오브젝트를 제거
+        }
+        else if(other.gameObject.tag == "Object")
+        {
+            audioSource.Play();
+            Instantiate(hitParticlePrefab1, transform.position, transform.rotation); // 총알 적중 지점에 연츨
+        }
+
+    }
+
+}
---
+++ BowAndArrow/Extra/Target.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour, IArrowHittable
+{
+    public float forceAmount = 1.0f;
+    public Material otherMaterial = null;
+    [SerializeField] GameObject hand;
+    public void Hit(Arrow arrow)
+    {
+        ApplyMaterial();
+        ApplyForce(arrow.transform.forward);
+    }
+
+    private void ApplyMaterial()
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material = otherMaterial;
+        hand.gameObject.SetActive(true);
+    }
+
+    private void ApplyForce(Vector3 direction)
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(direction * forceAmount);
+    }
+}
